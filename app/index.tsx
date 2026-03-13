3c5ac5384194a6ff95662192fd02d98f6dd4a1b9
arthsaAr <kcraashtra431qwe@gmail.com>
2026-03-13 00:49:48 -0600

    Welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,34 @@
 import { useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
+import { Ionicons } from "@expo/vector-icons";
+
 export default function InitialHomeScreen() {
   const router = useRouter();
 
   return (
     <View className="flex-1 justify-center items-center bg-white px-6">
-      <Text className="text-3xl font-bold text-red-500">FirstFive</Text>
-      <Text className="text-gray-500 mt-2">Help arrives before the ambulance!</Text>
+      
+      <View className="w-24 h-24 rounded-full bg-red-500 justify-center items-center">
+          <Ionicons name="heart" size={50} color="white" />
+      </View>
+
+      <Text className="text-5xl font-bold text-black mt-3 text-center">Welcome to FirstFive</Text>
+
+      <View className="flex-col justify-center items-center mt-3 text-center px-4">
+        <Text className="text-gray-500 text-xl mt-2 text-center">Connect with trained responders during emergencies</Text>
+        <Text className="text-gray-500 text-lg mt-2 text-center">Help arrives before the ambulance!</Text>
+      </View>
 
       <TouchableOpacity
         className="mt-10 w-full bg-red-500 py-4 rounded-2xl items-center"
         onPress={() => router.push("/responderSetup")}
       >
-        <Text className="text-white font-bold text-lg">I Want to Help</Text>
+        <Text className="text-white font-bold text-lg">I Want to Help(Responder)</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
-        className="mt-4 w-full bg-gray-100 py-4 rounded-2xl items-center"
+        className="mt-4 w-full bg-gray-100 py-4 rounded-2xl  border border-gray-500 items-center"
         onPress={() => router.push("/home")}
       >
         <Text className="text-gray-700 font-bold text-lg">Continue as User</Text>
